40d8415962dbcb7b9f66c4a288a02c6becb244b9
JonniBig <bigun.i88@gmail.com>
2023-09-18 21:37:26 +0300

    start2

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,18 @@
+import css from './Statistics.module.scss';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(item => (
+          <li className={css.item} key={item.id}>
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
